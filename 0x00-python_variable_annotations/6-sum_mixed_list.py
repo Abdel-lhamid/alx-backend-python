98833e49d37fa27c7e8ecc780ff94e38957358ab
ABDELHAMID El Yaagoubi <abdelhamidbusiness@gmail.com>
2024-08-15 17:02:19 +0100

    task 6

---
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""sum mixed list Module"""
+from typing import List, Union
+
+
+def def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
+    """return sum of mixed list int float"""
+    return float(sum(mxd_lst))
