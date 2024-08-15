0619232ffd98419866eb2b465b5cbc810a13f8eb
ABDELHAMID El Yaagoubi <abdelhamidbusiness@gmail.com>
2024-08-15 17:21:14 +0100

    task 10

---
+++ 0x00-python_variable_annotations/100-safe_first_element.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""safe_first_element Module"""
+from typing import Any, Sequence, Union
+
+
+def def safe_first_element(lst: Sequence[Any]) -> Union[Any, None]:
+    """safely returns the first elem of aq squence if exists"""
+    if lst:
+        return lst[0]
+    else:
+        return None
