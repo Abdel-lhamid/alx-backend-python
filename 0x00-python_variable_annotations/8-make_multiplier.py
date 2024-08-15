6883ec399e57601838c7d2c6ae066870969f7556
ABDELHAMID El Yaagoubi <abdelhamidbusiness@gmail.com>
2024-08-15 17:13:14 +0100

    task 8

---
+++ 0x00-python_variable_annotations/8-make_multiplier.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""make multiplier Module"""
+from typing import Callable
+
+
+def make_multiplier(multiplier: float) -> Callable[[float], float]:
+    """return a multiplier function"""
+    return lambda x:x * multiplier
