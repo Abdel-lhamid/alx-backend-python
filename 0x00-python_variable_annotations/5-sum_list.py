ea786bb160a95e9422698f161ef0ec393b4e1316
ABDELHAMID El Yaagoubi <abdelhamidbusiness@gmail.com>
2024-08-15 16:57:25 +0100

    task 5

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+""" sum list Module"""
+from typing import List
+
+
+def sum_list(input_list: List[float]) -> float:
+    """returns the sum of a float list"""
+    return sum(input_list)
